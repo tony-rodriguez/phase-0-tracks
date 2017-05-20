a1817bd72857dd11bbdc6e978c62b891ce83cf94
Tony Rodriguez <tonyr923@gmail.com>
2017-05-20 16:21:47 -0500

    Add pseudocode and algorithm for alias creator

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,53 @@
+# PSEUDOCODE
+# First take original string containing name and convert it into an array by splitting it where there is a space (' ')
+# Reverse the order of the array of the first and last name
+# Convert the names to be all lowercase letters
+# Split the names into an array of all individual characters
+# Create reference strings containing all consonants and all vowels
+# Loop through each letter in the name, check if it appears in either of the reference strings
+# Find index where the letter appears, add 1 for the index of the new letter
+# Rejoin all characters into a string
+# Capitalize first letter in each word
+
+def swap(full_name)
+  swapped_name = full_name.split(' ')
+  swapped_name.reverse!.join(' ')
+end
+
+def split_name(name)
+  name.downcase.chars
+end
+
+def next_letter(name_array = [])
+  # reference strings for vowels and consonants
+  vowels = "aeiou"
+  consonants = "bcdfghjklmnpqrstvwxyz"
+  name_array.map! do |letter|
+    if vowels.include? letter
+      if vowels.index(letter) == (vowels.length - 1)
+        #returns first vowel if letter is last vowel (edge condtion)
+        vowels[0]
+      else vowels[vowels.index(letter) + 1]
+      end
+    elsif consonants.include? letter
+      if consonants.index(letter) == (consonants.length - 1)
+        # edge condition for consonants
+        consonants[0]
+      else consonants[consonants.index(letter) + 1]
+      end
+    else letter
+    end
+  end
+end
+
+def rejoin(name_arr = [])
+  final = name_arr.join('').split(' ')
+  final.map! { |word| word.capitalize }
+  final.join(' ')
+end
+
+def create_alias(name)
+  rejoin(next_letter(split_name(swap(name))))
+end
+
+p create_alias("Felicia Torres")
