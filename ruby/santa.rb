ca6ff4120235da68e24fc64232143c97d0e28740
Tony Rodriguez <tonyr923@gmail.com>
2017-05-24 22:20:41 -0500

    create Santa class with basic instance methods

---
+++ ruby/santa.rb
@@ -0,0 +1,17 @@
+class Santa
+  def initialize
+    puts "Initializing Santa instance..."
+  end
+
+  def speak
+    puts "Ho, ho, ho! Haaaappy holidays!"
+  end
+
+  def eat_milk_and_cookies(cookie)
+    puts "That was a good #{cookie}!"
+  end
+end
+
+klaus = Santa.new
+klaus.eat_milk_and_cookies("oatmeal raisin cookie")
+klaus.speak
