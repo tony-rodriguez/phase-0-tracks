b850417f2636879a78c17d25b73282dad4975445
Tony Rodriguez <tonyr923@gmail.com>
2017-05-26 14:58:09 -0500

    Convert shout module to mixin version and include it in new classes for person and parrot

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,38 @@
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yelling_happily(words)
+#     words.upcase + "!" + " :)"
+#   end
+# end
+
+# p Shout.yell_angrily("I can't stand you")
+# p Shout.yelling_happily("I got the job")
+
 module Shout
-  def self.yell_angrily(words)
+  def yell_angrily(words)
     words + "!!!" + " :("
   end
 
-  def self.yelling_happily(words)
+  def yell_happily(words)
     words.upcase + "!" + " :)"
   end
 end
 
-p Shout.yell_angrily("I can't stand you")
-p Shout.yelling_happily("I got the job")
+class Person
+  include Shout
+end
+
+class Parrot
+  include Shout
+end
+
+new_grad = Person.new
+polly = Parrot.new
+
+puts new_grad.yell_happily("I did it")
+puts new_grad.yell_angrily("My hair is a mess")
+puts polly.yell_angrily("feed me")
+puts polly.yell_happily("thank you")
