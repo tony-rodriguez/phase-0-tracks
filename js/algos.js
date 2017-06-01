8a22ceb9b08c9dd6fea93118a701c7c683bfc368
Tony Rodriguez <tonyr923@gmail.com>
2017-06-01 16:40:55 -0500

    Pseudocode function to find the longest phrase in array of strings

---
+++ js/algos.js
@@ -0,0 +1,10 @@
+/* ---Finding the Longest Phrase---
+- Input: an array
+- Output: a string
+
+- First, create an empty string variable to contain the longest phrase
+- Then, initiate a loop to iterate over each item in the array
+- Compare the length of each item in the array to what is contained in the 'longest phrase' variable
+- IF the item at the current iteration is longer than what is in the string, reassign the string to contain the current item
+- return the 'longest phrase' variable
+*/
