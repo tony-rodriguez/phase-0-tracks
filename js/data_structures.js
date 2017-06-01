b4116bd5f264dbd1cce370585fe3983f0aac43f7
Tony Rodriguez <tonyr923@gmail.com>
2017-06-01 12:30:44 -0500

    Create object with horses and colors

--- js/data_structures.js
+++ js/data_structures.js
@@ -4,5 +4,13 @@ var horses = ["Ed", "Longtail", "Leggy", "Gandalf"];
 colors.push("brown");
 horses.push("Seabiscuit");
 
-console.log(colors);
-console.log(horses);
+// console.log(colors);
+// console.log(horses);
+
+var barn = {}
+
+for(i = 0; i < colors.length; i++) {
+  barn[horses[i]] = colors[i]
+}
+
+console.log(barn)
