38c7a5c9ea4230f73b1b64c277a1ee9330c3c5e6
Tony Rodriguez <tonyr923@gmail.com>
2017-06-01 12:18:03 -0500

    Add arrays of colors and horses

---
+++ js/data_structures.js
@@ -0,0 +1,8 @@
+var colors = ["blue", "red", "green", "grey"];
+var horses = ["Ed", "Longtail", "Leggy", "Gandalf"];
+
+colors.push("brown");
+horses.push("Seabiscuit");
+
+console.log(colors);
+console.log(horses);
