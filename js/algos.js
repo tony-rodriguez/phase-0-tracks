d20d4c1c2f693452bed255fffe47168123fdb7e4
Tony Rodriguez <tonyr923@gmail.com>
2017-06-02 18:11:06 -0500

    Implement and test function to find longest phrase

--- js/algos.js
+++ js/algos.js
@@ -8,3 +8,28 @@
 - IF the item at the current iteration is longer than what is in the string, reassign the string to contain the current item
 - return the 'longest phrase' variable
 */
+
+function longestPhrase(phrases) {
+  var longest = '';
+  for(i=0; i < phrases.length; i++) {
+    if (phrases[i].length > longest.length) {
+      longest = phrases[i]
+    }
+  }
+  return longest;
+}
+
+
+// Test code
+var test1 = ["long phrase","longest phrase","longer phrase"];
+var test2 = ["this is a phrase","here is another","this is also a phrase","one more"];
+var test3 = ["dog","cat","mouse","rhinocerous","rabbit","horse","elephant"];
+
+console.log(test1);
+console.log(longestPhrase(test1));
+
+console.log(test2);
+console.log(longestPhrase(test2));
+
+console.log(test3);
+console.log(longestPhrase(test3));
