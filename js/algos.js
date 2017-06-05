0adb7b75c30ad9fe0418cb9d216b51912e19e1bb
Tony Rodriguez <tonyr923@gmail.com>
2017-06-04 23:24:39 -0500

    Pseudocode and implement function to create array of random strings

--- js/algos.js
+++ js/algos.js
@@ -13,11 +13,21 @@ Output: a string
 Input: 2 Objects
 Output: Boolean
 
-
 - For both objects, store all keys and all values in separate arrays
 - Iterate through the array of keys and array of values of the first object, checking for matches in the array of keys and array of values of the second object
 - If at any point a match is found, break out of loops and return true
 - If all keys and values are checked and no matches are found, return false
+*/
+// ------------------------------------------------------------------
+/* ---Generating Array of Random Strings---
+Input: An Integer
+Output: Array of strings
+
+- Start by creating empty array to hold random strings
+- Create for loop that will iterate based on the argument passed in
+- Randomly determine length of each word (min 1 letter, max 10)
+- add random letters into string
+- push string to array
 */
 
 function longestPhrase(phrases) {
@@ -44,6 +54,20 @@ function objectMatchChecker(ob1, ob2) {
   return false
 }
 
+function randomStrings(iterations) {
+  var strings = [];
+  var possible_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  for(i = 0; i < iterations; i++) {
+    var string_length = Math.floor((Math.random() * 10) + 1);
+    var random_string = '';
+    for(j = 0; j < string_length; j++) {
+      random_string += possible_chars[Math.floor((Math.random() * possible_chars.length))]
+    }
+    strings.push(random_string)
+  }
+  return strings
+}
+
 
 // Test code for longestPhrase function
 // var test1 = ["long phrase","longest phrase","longer phrase"];
@@ -60,9 +84,11 @@ function objectMatchChecker(ob1, ob2) {
 // console.log(longestPhrase(test3));
 
 // Test code for objectMatchChecker function
-var testobject1 = {color: "blue", size: "small", name: "Sparky"};
-var testobject2 = {shape: "flat", quantity: "none"}
+// var testobject1 = {color: "blue", size: "small", name: "Sparky"};
+// var testobject2 = {shape: "flat", quantity: "none"}
 
-console.log(objectMatchChecker(testobject1, testobject2))
-console.log(objectMatchChecker({name: "Steven", age: 54}, {name: "Tamir", age: 54}));
-console.log(objectMatchChecker({animal: "Dog", legs: 4}, {animal: "Dog", legs: 3}));
+// console.log(objectMatchChecker(testobject1, testobject2))
+// console.log(objectMatchChecker({name: "Steven", age: 54}, {name: "Tamir", age: 54}));
+// console.log(objectMatchChecker({animal: "Dog", legs: 4}, {animal: "Dog", legs: 3}));
+
+console.log(randomStrings(10))
