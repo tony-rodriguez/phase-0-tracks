1385b2af38c79c906400f30e95103f062d78c807
Tony Rodriguez <tonyr923@gmail.com>
2017-06-12 10:30:22 -0500

    Add code to successfully create blog database

--- blog-app/blog_interface.rb
+++ blog-app/blog_interface.rb
@@ -1,1 +1,15 @@
 require 'sqlite3'
+
+db = SQLite3::Database.new('blog.db')
+
+create_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS blog(
+    id INTEGER PRIMARY KEY,
+    title VARCHAR(255),
+    author VARCHAR(255),
+    post_date TEXT,
+    content VARCHAR(255)
+  )
+SQL
+
+db.execute(create_table_cmd)
