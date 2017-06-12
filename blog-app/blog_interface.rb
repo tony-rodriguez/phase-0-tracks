3822dc7a193458bfd0ec75331be7e30d9922d008
Tony Rodriguez <tonyr923@gmail.com>
2017-06-12 17:25:12 -0500

    Add control flow and dummy method for adding blog post

--- blog-app/blog_interface.rb
+++ blog-app/blog_interface.rb
@@ -25,7 +25,7 @@ def display_menu
   puts "-"*35
   puts "Read - see previous blog posts"
   puts "Write - create your own blog post"
-  puts "Search - look for specific blog posts"
+  puts "Search - look for a specific blog post"
   puts "Edit - change something about a post"
   puts "Remove - delete a post of yours"
   puts "Quit - leave this blog"
@@ -35,10 +35,18 @@ end
 def display_posts(database)
   blog_data = database.execute('SELECT * FROM blog')
   blog_data.each do |post|
+    # puts '-'*35
     puts "Post No.: #{post['id']} Date: #{post['post_date']} Title: #{post['title']} "
     puts "Author: #{post['author']}"
     puts post['content']
+    puts '-'*35
   end
+end
+
+def new_post(database, name, title, content)
+  puts "name: #{name}"
+  puts "title: #{title}"
+  puts "content: #{content}"
 end
 
 puts "Hi! Welcome to my blog. Below is the menu. If you'd like to see this menu again later, type in 'menu'."
@@ -49,6 +57,25 @@ until input == 'quit' do
     display_menu
   elsif input == 'read'
     display_posts(db)
+  elsif input == 'write'
+    puts '-'*35
+    puts "Thank you for contributing! The following prompts will guide you through the process of authoring a post. Type 'cancel' at any step to escape this process."
+    puts
+    puts "How would you like your name to appear (as author)?"
+    author_name = gets.chomp
+    unless author_name.downcase == 'cancel'
+      puts "What would you like the title of your post to be?"
+      title = gets.chomp
+    end
+    # must check for nil in the event that user enters cancel before prompted for title or content
+    unless title.nil? || title.downcase == 'cancel'
+      puts "Paste or type your content:"
+      content = gets.chomp
+    end
+    unless content.nil? || content.downcase == 'cancel'
+      new_post(db, author_name, title, content)
+    end
+  else puts "Sorry, your input doesn't seem to be valid. Refer to the menu for valid options. Please try again: "
   end
   input = gets.chomp.downcase
 end
