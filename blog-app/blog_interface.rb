88ec2d2d858ef99fd3d50313365790ca701fb894
Tony Rodriguez <tonyr923@gmail.com>
2017-06-12 11:24:17 -0500

    Add test data to database and add method to read blog posts

--- blog-app/blog_interface.rb
+++ blog-app/blog_interface.rb
@@ -1,6 +1,7 @@
 require 'sqlite3'
 
 db = SQLite3::Database.new('blog.db')
+db.results_as_hash = true
 
 create_table_cmd = <<-SQL
   CREATE TABLE IF NOT EXISTS blog(
@@ -13,3 +14,41 @@ create_table_cmd = <<-SQL
 SQL
 
 db.execute(create_table_cmd)
+
+# insert_test = <<-SQL
+#   INSERT INTO blog (title, author, post_date, content) VALUES
+#   ('First Post!', 'Tony Rodriguez', date('now'), "I hope this works!")
+# SQL
+
+def display_menu
+  puts "Menu options: type the keyword of your choice"
+  puts "-"*35
+  puts "Read - see previous blog posts"
+  puts "Write - create your own blog post"
+  puts "Search - look for specific blog posts"
+  puts "Edit - change something about a post"
+  puts "Remove - delete a post of yours"
+  puts "Quit - leave this blog"
+  puts "-"*35
+end
+
+def display_posts(database)
+  blog_data = database.execute('SELECT * FROM blog')
+  blog_data.each do |post|
+    puts "Post No.: #{post['id']} Date: #{post['post_date']} Title: #{post['title']} "
+    puts "Author: #{post['author']}"
+    puts post['content']
+  end
+end
+
+puts "Hi! Welcome to my blog. Below is the menu. If you'd like to see this menu again later, type in 'menu'."
+display_menu
+input = gets.chomp.downcase
+until input == 'quit' do
+  if input == 'menu'
+    display_menu
+  elsif input == 'read'
+    display_posts(db)
+  end
+  input = gets.chomp.downcase
+end
